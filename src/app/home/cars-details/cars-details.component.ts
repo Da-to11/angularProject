3e061255e981de7f4c4952b70f418b2c11a279e1
Default <papavadato89@gmail.com>
2025-05-09 14:45:16 -0700

    project upd 3

---
+++ src/app/home/cars-details/cars-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cars-details',
+  imports: [],
+  templateUrl: './cars-details.component.html',
+  styleUrl: './cars-details.component.scss'
+})
+export class CarsDetailsComponent {
+
+}
